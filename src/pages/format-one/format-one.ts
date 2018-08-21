e9d3ce3e95aaa78cc1a50af0e4e329638ceed289
soxmomomo <evamore121@hotmail.com>
2018-08-21 22:57:59 +0800

    remove package-lock.json

---
+++ src/pages/format-one/format-one.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+    selector: 'format-one-page',
+    templateUrl: 'format-one.html'
+})
+export class FormatOnePage {
+
+    constructor(public navCtrl: NavController) {
+    }
+
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FormatOnePage } from '../format-one/format-one';
 
 @Component({
-  selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
   }
 
+  clickOne() {
+    this.navCtrl.push(FormatOnePage);
+  }
 }
